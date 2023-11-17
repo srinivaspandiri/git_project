1ffc4b01a9f898f5a099ce1b61637195e8f50f8b
srinivasreddy <srinivasreddyp14@gmail.com>
2023-11-17 16:05:59 +0100

    my first version

---
+++ DAY1.sql
@@ -0,0 +1,20 @@
+show databases;
+
+create database weekday_4to6;
+
+use weekday_4to6;  /* when ever we are working on this databse we need to do this command */
+
+create table pet(
+F_name varchar(20),
+owner_name varchar(20),
+species varchar(20),
+gender char(1),
+birth date,
+death date
+);
+
+show tables;
+
+describe pet;
+
+show columns from pet;
